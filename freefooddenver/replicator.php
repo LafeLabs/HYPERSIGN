f7d4e1557b349d60ea0dc3ca5cda7c6b0906fb2c
Trash Robot <lafelabs@gmail.com>
2023-07-27 19:47:44 -0600

    freefood denver and also shittr/

---
+++ freefooddenver/replicator.php
@@ -0,0 +1,57 @@
+<?php
+
+$dnaurl = "https://raw.githubusercontent.com/LafeLabs/HYPERSIGN/main/data/dna.txt";
+
+if(isset($_GET["dna"])){
+    $dnaurl = $_GET["dna"];
+}
+
+
+$baseurl = explode("data/",$dnaurl)[0];
+$dnaraw = file_get_contents($dnaurl);
+$dna = json_decode($dnaraw);
+
+mkdir("data");
+mkdir("php");
+mkdir("trashmagic");
+
+copy("https://raw.githubusercontent.com/LafeLabs/HYPERSIGN/main/php/replicator.txt","replicator.php");
+
+foreach($dna->html as $value){
+    
+    copy($baseurl.$value,$value);
+
+}
+
+
+foreach($dna->data as $value){
+    
+    copy($baseurl."data/".$value,"data/".$value);
+    
+}
+
+
+
+foreach($dna->php as $value){
+ 
+    copy($baseurl."php/".$value,"php/".$value);
+    copy($baseurl."php/".$value,explode(".",$value)[0].".php");
+
+}
+    
+    
+
+
+?>
+<a href = "index.html">CLICK TO GO TO NEW PAGE</a>
+<style>
+body{
+    background-color:#9f8767;
+    font-family:Comic Sans MS;
+    font-size:3em;
+}
+
+a{
+    font-size:3em;
+}
+</style>
